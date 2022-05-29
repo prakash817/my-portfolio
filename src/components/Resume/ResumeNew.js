4c1b10e79b7331a08191c64e02512aaa8b4aa607
Prakash Gupta <96722484+prakash817@users.noreply.github.com>
2022-05-29 16:05:16 +0530

    width

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -48,7 +48,7 @@ function ResumeNew() {
         {/* <Document file={linkKumar} className="d-flex justify-content-center">
             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
           </Document> */}
-        <img src={resumePic} />
+        <img src={resumePic} style={{ maxWidth: "100vw" }}/>
         {/* </Row> */}
 
         <Row
