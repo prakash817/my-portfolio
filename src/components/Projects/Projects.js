43dde37aba51b3dac7b8f1f99a17523d48d47ea7
Prakash Gupta <96722484+prakash817@users.noreply.github.com>
2022-05-29 16:16:40 +0530

    updated project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,9 +5,9 @@ import Particle from "../Particle";
 import leaf from "../../Assets/Projects/leaf.png";
 import emotion from "../../Assets/Projects/emotion.jpeg";
 import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
+import meesho from "../../Assets/Projects/meesho.png";
 import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import netmeds from "../../Assets/Projects/netmeds.png";
 
 function Projects() {
   return (
@@ -23,21 +23,21 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={meesho}
               isBlog={false}
-              title="Chatify"
+              title="meesho"
               description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              link="https://github.com/prakash817/Chatify"
+              link="https://github.com/prakash817/meesho"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={netmeds}
               isBlog={false}
-              title="Bits-0f-C0de"
+              title="netmeds"
               description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              link="https://github.com/prakash817/Bits-0f-C0de"
+              link="https://github.com/prakash817/netmedspharmencyclone"
             />
           </Col>
 
